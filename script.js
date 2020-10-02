ae5e5746c5ea1b40f99a0f4f8fb09ff1753ed8c0
Hervé Romary <h.romary@gmail.com>
2020-10-02 09:05:42 +0200

    carousel 1.0

---
+++ script.js
@@ -0,0 +1,94 @@
+//carousel
+const track = document.querySelector(".carousel_track");
+const slides = Array.from(track.children);
+const nextButton = document.querySelector(".carousel_button--right");
+const prevButton = document.querySelector(".carousel_button--left");
+const dotsNav = document.querySelector(".carousel_nav");
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+//arrange the slides next to one another
+slides[0].style.left = slideWidth * 0 + "px";
+slides[1].style.left = slideWidth * 1 + "px";
+slides[2].style.left = slideWidth * 2 + "px";
+
+// const setSlidePosition = (slide, index) => {
+//   slide.style.left = slideWidth * index + "px";
+// };
+// slide.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+  currentSlide.classList.remove("current-slide");
+  targetSlide.classList.add("current-slide");
+};
+
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove("current-slide");
+  targetDot.classList.add("current-slide");
+};
+
+// const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+//     if (targetIndex === 0) {
+//       prevButton.classList.add("is-hidden");
+//       nextButton.classList.remove("is-hidden");
+//     } else if (targetIndex === slides.length - 1) {
+//       prevButton.classList.remove("is-hidden");
+//       nextButton.classList.add("is-hidden");
+//     } else {
+//       prevButton.classList.remove("is-hidden");
+//       nextButton.classList.add("is-hidden");
+//   }
+
+// when I click left move slides to the left
+prevButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const prevDot = currentDot.previousElementSibling;
+  //   const prevIndex = slides.findIndex((slides) => slide === prevSlide);
+  updateDots(currentDot, prevDot);
+  moveToSlide(track, currentSlide, prevSlide);
+  //   hideShowArrows(slides, prevButton, nextButton, targetIndex);
+});
+// when I click right move slides to the right
+nextButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const nextDot = currentDot.nextElementSibling;
+  //   const nextIndex = slides.findIndex((slides) => slide === nextSlide);
+  moveToSlide(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
+  //   hideShowArrows(slides, prevButton, nextButton, targetIndex);
+});
+
+//when I click the nav indicator, more to that slide
+
+dotsNav.addEventListener("click", (e) => {
+  //what indicator was clicked on?
+  const targetDot = e.target.closest("button");
+
+  if (!targetDot) return;
+
+  const currentSlide = track.querySelector(".current-slide");
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const targetIndex = dots.findIndex((dot) => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+
+  moveToSlide(track, currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);
+  // hideShowArrows(slides, prevButton, nextButton, targetIndex)
+
+  //   if (targetIndex === 0) {
+  //     prevButton.classList.add("is-hidden");
+  //     nextButton.classList.remove("is-hidden");
+  //   } else if (targetIndex === slides.length - 1) {
+  //     prevButton.classList.remove("is-hidden");
+  //     nextButton.classList.add("is-hidden");
+  //   } else {
+  //     prevButton.classList.remove("is-hidden");
+  //     nextButton.classList.add("is-hidden");
+  //   }
+});
